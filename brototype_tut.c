71faf6104f820c9bd29c591bc668f028a2e5b5b6
OMG DINO <>
2022-07-16 13:19:36 +0000

    made a program to print memory location

--- brototype_tut.c
+++ brototype_tut.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-int main(void){
-  int a = 1,b=2;
-  a=b+a;
-  b=a-b;
-  a=a-b;
-  printf("New values are: a=%d,b=%d,",a,b);
-return 0;
+ #include <stdio.h>
+
+int main() {
+	int a = 10;
+	int *b;
+	b = &a;
+	printf("%p", b);
+
+	return 0;
 }
-  
