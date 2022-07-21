5b9fa41f20f68a624ff5d2a814f1cc7bb490bc79
OMG DINO <>
2022-07-21 17:21:20 +0000

    simple calculator with if,if else

--- main.c
+++ main.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 
 int main(void){
-  printf("suii");
-return 0;
-
+  int num,num2,op,sum,sub,div,multi;
+  printf("Enter two numbers :");
+  scanf("%d""%d",&num,&num2);
+  printf("1)+\n2)-\n3)/\n4)*\nEnter the opertaion you want to execute:");
+  scanf("%d",&op);
+if(op == 1)
+{
+  sum = num +num2;
+  printf("Your answer is %d",sum);
 }
-
-
+  else if (op == 2)
+  {
+    sub = num-num2;
+    printf("Your answer is %d",sub);
+  }
+  else if (op == 3)
+  {
+    div = num/num2;
+    printf("Your answer is %d",div);
+  }else if (op == 4)
+  {
+    multi = num *num2;
+    printf("Your answer is %d",multi);
+  }
+}
