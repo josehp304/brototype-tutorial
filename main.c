f74ea6c19c667c74c13b5923d8e65fe41be4550f
joseph babu <83821474+josehp304@users.noreply.github.com>
2022-07-28 08:13:16 +0000

    Sum of odd num using for loop

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main(void){
-  int i,num,multi;
-printf("Enter number: ");
-scanf("%d",&num);
-for(i=1;i<=10;i++)
-  {
-    multi = num*i;
-    printf("%d X%d = %d\n",i,num,multi);
-  }
+int lim,i,sum=0;
+  printf("Enter a number:");
+  scanf("%d",&lim);
+  for (i=1;i<=lim;i=i+2)
+    {
+      sum = sum+i;
+    }
+printf("sum of odd no is =%d",sum);
 return 0;
-}
+} 
+
