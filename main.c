e4ed599e198014a45be4fa3217fae8760bf39eaa
OMG DINO <83821474+josehp304@users.noreply.github.com>
2022-07-28 06:45:59 +0000

    multiplication table using for loop

--- main.c
+++ main.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 
 int main(void){
-  int choice;
-  printf("1)RTX 3090ti\n2)Intel i9 900k\n3)Rysen 5 5000\n4)PS5\nSelect a order from the above :");
-scanf("%d",&choice);
- switch(choice){
-   case 1:
-    printf("Your RTX 3090 will be coming soon");
-    break;
-   case 2:
-   printf("Your INTEL i9 900k will be coming soon");
-   break;
-   case 3:
-   printf("Your Rysen 5 5000 will be coming soon");
-   break; 
-   case 4:
-   printf("Your PS5 will be coming soon");
-   break;
-   default:
-   printf("incorrect input");
-   
+  int i,num,multi;
+printf("Enter number: ");
+scanf("%d",&num);
+for(i=1;i<=10;i++)
+  {
+    multi = num*i;
+    printf("%d X%d = %d\n",i,num,multi);
   }
 return 0;
 }
