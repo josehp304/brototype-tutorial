01044639965065f6808da442c29984d184756353
joseph babu <josehp304@gmail.com>
2022-08-04 17:12:12 +0530

    Transfers all the values from array 1 to array 2

--- main.c
+++ main.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
-
-int main(void)
-{
-int lim,i,j,temp,value[100];
-printf("Enter a limit for values:");
+#include <stdlib.h>
+int main(void){
+int array1[100],array2[100],temp[100],i,j,lim;
+//Collecting array limit and array values
+printf("Enter the size of the array:");
 scanf("%d",&lim);
-printf("Enter the values:");
+printf("Enter the values of array 1\n");
+for (i=0;i<lim;i++)
+    {
+        scanf("%d",&array1[i]);
+    }
+printf("Enter the values of array 2\n");
+for (i=0;i<lim;i++)
+    {
+        scanf("%d",&array2[i]);    
+    }
+//Transfering values from array 1 to 2
 for(i=0;i<lim;i++)
     {
-    scanf("%d",&value[i]);
+        temp[i]=array1[i];
+        array1[i]=array2[i];
+        array2[i]=temp[i];
     }
-for(i=0;i<lim;i++)
-{
-    for(j=i+1;j<lim;j++)
-    {
-        if(value[i]>value[j])
-        {
-        temp=value[i];
-        value[i]=value[j];
-        value[j]=temp;
-        }
-    }
-}
-printf("Sorted values are:");
+//printing result
+printf("Values of array1: ");
 for(i=0;i<lim;i++)
     {
-        printf("%d\n",value[i]);
+        printf("%d,",array1[i]);
     }
+printf("\n");
+printf("Values of array 2: ");
+for (i=0;i<lim;i++)
+    {
+        printf("%d,",array2[i]);
+    }
+
+
+
 return 0;
 }
 
