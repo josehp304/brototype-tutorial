6f08a786b0fc3577bc6a4805292c38b95e615ac9
joseph babu <josehp304@gmail.com>
2022-08-06 14:31:05 +0530

    number of even numbers in array

--- main.c
+++ main.c
@@ -1,41 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(void){
-int array1[100],array2[100],temp[100],i,j,lim;
-//Collecting array limit and array values
-printf("Enter the size of the array:");
+int i,array[100],lim,even_no=0;
+printf("Enter the size of array ");
 scanf("%d",&lim);
-printf("Enter the values of array 1\n");
 for (i=0;i<lim;i++)
-    {
-        scanf("%d",&array1[i]);
-    }
-printf("Enter the values of array 2\n");
+{
+    scanf("%d",&array[i]);
+}
 for (i=0;i<lim;i++)
+{
+    if (array[i]%2==0)
     {
-        scanf("%d",&array2[i]);    
+        even_no=even_no+1;
     }
-//Transfering values from array 1 to 2
-for(i=0;i<lim;i++)
-    {
-        temp[i]=array1[i];
-        array1[i]=array2[i];
-        array2[i]=temp[i];
-    }
-//printing result
-printf("Values of array1: ");
-for(i=0;i<lim;i++)
-    {
-        printf("%d,",array1[i]);
-    }
-printf("\n");
-printf("Values of array 2: ");
-for (i=0;i<lim;i++)
-    {
-        printf("%d,",array2[i]);
-    }
-
-
+}
+printf("Number of even numbers are: %d",even_no);
 
 return 0;
 }
