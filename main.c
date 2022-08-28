98754ed6cd958ac78ac8da07c173da8a825a01f3
joseph babu <josehp304@gmail.com>
2022-08-28 22:16:54 +0530

    palidrome project

--- main.c
+++ main.c
@@ -1,34 +1,31 @@
+
 #include <stdio.h>
-#include <windows.h>
-#include <stdlib.h>
-int main(){
-    int h,m,s,gap;
-    gap = 1000;
-    printf("Enter your time :\n");
-    scanf("%d%d%d",&h,&m,&s);
-    if(h>12|| m>60|| s>60){
-        printf("error \n");
-        exit(0);
+int main()
+{
+    char str[20];
+    int i,j,k=1;
+
+    printf("Enter a name plz:");
+    scanf("%s",str);
+    for (i=0;str[i]!='\0';i++);  
+
+    for (j=0;j<=(i/2);j++){
+        if (str[j]!=str[i-(1+j)]){
+            k=k+1;
+            break;
     }
-    while (1)
-    {
-        s++;
-        if(s==60){
-            m++;
-            s=0;
-        }
-        if(m==60){
-            h++;
-            m=0;
-        }
-        if(h>12){
-            h=1;
-        }
-        printf("\n clock: \n");
-        printf("\n %02d:%02d:%02d",h,m,s);
-        Sleep(gap);
-        system("cls");
+    }
+
+    if (k=1){
+        printf("The word is palidrome\n");
 
     }
-    
+    else{
+        printf("the word is not a palodrome %d",k);
+        }
+return 0;
 }
+
+
+
+
