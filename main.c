099cd4cc40d61cd0f49d91e01d04bfd2167afc49
joseph babu <josehp304@gmail.com>
2022-08-31 18:07:30 +0530

    Addition of 2d array

--- main.c
+++ main.c
@@ -1,31 +1,36 @@
-
 #include <stdio.h>
-int main()
-{
-    char str[20];
-    int i,j,k=0;
+#include <stdlib.h>
+int main(void){
+    int lim,array1[100][100],array2[100][100],array_sum[100][100],i,j;
 
-    printf("Enter a name plz:");
-    scanf("%s",str);
-    for (i=0;str[i]!='\0';i++);  
 
-    for (j=0;j<=(i/2);j++){
-        if (str[j]!=str[i-(1+j)]){
-            k=k+1;
-            break;
-    }
-    }
-
-    if (k==0){
-        printf("The word is palidrome\n");
-
-    }
-    else{
-        printf("the word is not a palodrome %d",k);
+    printf("enter the size of the array:\n");
+    scanf("%d",&lim);
+    printf("enter the values of first array:\n ");
+    for(i=0;i<lim;i++){
+        for(j=0;j<lim;j++){
+            scanf("%d",&array1[i][j]);
         }
-return 0;
+    }
+    printf("enter the values of second array: \n");
+    for(i=0;i<lim;i++){
+        for(j=0;j<lim;j++){
+            scanf("%d",&array2[i][j]);
+        }
+    }
+
+
+    for(i=0;i<lim;i++){
+        for(j=0;j<lim;j++){
+            array_sum[i][j] = array1[i][j]+array2[i][j];
+        }
+    }
+
+    printf("The sum of arrays is:\n");
+    for(i=0;i<lim;i++){
+        for(j=0;j<lim;j++){
+            printf("%d \n",array_sum[i][j]);
+        }
+    }
+    return 0;
 }
-
-
-
-
